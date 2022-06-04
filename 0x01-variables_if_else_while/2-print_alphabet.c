9a114f0808e9ddb57693881d2bfe8fc47df357b8
benmatrood <konateyaya526@gmail.com>
2022-06-04 12:29:27 -0700

    2. I sometimes suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+int num;
+num = 97;
+while (num <= 122)
+putchar(num);
+num++;
+
+
+int p;
+p = 97;
+while (p <= 122)
+{
+putchar(p);
+p++;
+}
+
+putchar('\n');
+return (0);
+}
+
